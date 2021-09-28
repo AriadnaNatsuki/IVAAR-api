a276cbcd2851f56e3279ac9ae95714a7fc136b54
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-09-28 19:56:11 +0200

    Installing packages and first folders

---
+++ bin/seeds.js
@@ -0,0 +1,25 @@
+require("../config/db.config")
+const mongoose = require('mongoose')
+const Animal = require('../models/Animal.model')
+// const Animal=require('../models/Animal.model')
+let Animals = []
+
+mongoose.connection.once('open', () => {
+    console.info(`Connected to the database ${mongoose.connection.db.databaseName}`)
+})
+mongoose.connection.db
+    .dropDatabase()
+    .then(() => console.log("Database cleared"))
+return Animal.insertMany(Animals)
+    .then((animalsCreated) => console.log(`${animalsCreated.length} have been created`))
+    .catch(e => console.error('Error disconencting from DB', e))
+    .finally(() => {
+        mongoose.connection.close()
+            .then(() => console.log('Finish seeds.js'))
+            .catch(e => console.error(e))
+            .finally(() => {
+                process.exit(0)
+            })
+    })
+
+
